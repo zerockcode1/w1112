1be17bef1c7c83a6edfdbcc300c7721f8bd1f7d2
ZerockCode <zkang075@gmail.com>
2021-11-12 16:12:48 +0900

    로또 이벤트 시작

--- js/lotto.js
+++ js/lotto.js
@@ -3,3 +3,11 @@ console.log("lotto...................")
 console.log("lotto...................")
 console.log("lotto...................")
 console.log("lotto...................")
+
+const btn = document.getElementById("btn")
+
+console.log(btn)
+
+btn.addEventListener("click",(event) => {
+    console.log(event)
+})
