2f0820531117af88db9d810944af450e2564d67f
ZerockCode <zkang075@gmail.com>
2021-11-12 17:09:16 +0900

    로또 버전1

--- js/lotto.js
+++ js/lotto.js
@@ -10,4 +10,28 @@ console.log(btn)
 
 btn.addEventListener("click",(event) => {
     console.log(event)
-})
+
+    const target = document.getElementById("resultDiv")
+
+    const balls = makeLottoNum()
+
+    //clear all <h3> tags
+    target.innerHTML =''
+
+    for (const num of balls) {
+        const tag = document.createElement("h3")
+        const textNode = document.createTextNode(num)
+        tag.appendChild(textNode)
+        target.appendChild(tag)
+    }
+
+
+}, false)
+
+
+function makeLottoNum(){
+
+    const numArr = [2,13,21,32,35,40]
+
+    return numArr
+}
