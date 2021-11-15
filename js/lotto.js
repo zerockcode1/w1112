2eb8278e87a95bbd684139169a0e38ca864c3c9c
ZerockCode <zkang075@gmail.com>
2021-11-15 10:59:46 +0900

    로또 버전1

--- js/lotto.js
+++ js/lotto.js
@@ -31,7 +31,32 @@ btn.addEventListener("click",(event) => {
 
 function makeLottoNum(){
 
-    const numArr = [2,13,21,32,35,40]
+    const arr = []
 
-    return numArr
+    for (let i = 0; i < 6; i++) {
+        const tempNum = parseInt(Math.random() * 45) + 1
+        //arr안에 tempNum이 있다면
+        if( arr.indexOf(tempNum) >= 0 ){
+            console.log("중복")
+            i--
+            continue
+        }
+        arr.push(tempNum)
+    }
+
+    // arr.sort(function(a,b) {
+    //     return a -b
+    // })
+
+    arr.sort((a,b) => a - b )
+
+    return arr
+
 }
+
+
+
+
+
+
+
